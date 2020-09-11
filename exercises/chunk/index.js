5d9fe464e85dc1a6ab7e6ddffd0896f1859a0f09
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-09-11 11:38:51 -0500

    solved!

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,68 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// Solution 1 (his way)
+// function chunk(array, size) {
+//   const chunked = [];
+//   for (let elem of array) {
+//     const lastElem = chunked[chunked.length - 1];
+//     if (!lastElem || lastElem.length === size) {
+//       // putting a new chunk WITH the element already in it
+//       chunked.push([elem]);
+//     } else {
+//       lastElem.push(elem);
+//     }
+//   }
+
+//   return chunked;
+// }
+
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// Solution 2 (my attempt at his way with hints) (so close!!)
+// function chunk(array, size) {
+//   const chunked = [];
+//   let idx = 0;
+//   while (idx < array.length) {
+//     chunked.push(array.slice(idx, size));
+//     idx += size;
+//   }
+//   return chunked;
+// }
+
+// Solution 2 (his way)
+// function chunk(array, size) {
+//   const chunked = [];
+//   let idx = 0;
+//   while (idx < array.length) {
+//     chunked.push(array.slice(idx, idx + size));
+//     idx += size;
+//   }
+//   return chunked;
+// }
+
+// Solution 3 (found on the interwebz) using sPlice
+// function chunk(array, size) {
+//   const chunked = [];
+//   let arrayCopy = [...array];
+//   // let idx = 0;
+//   while (0 < arrayCopy.length) {
+//     chunked.push(arrayCopy.splice(0, size));
+//   }
+//   return chunked;
+// }
+
+// Solution 4 (found on the interwebz) using recursion
+/*
+In this approach, we consistently reduce the problem set(i.e the size of the array) by slicing off chunks of the required size until all elements within the array have been grouped. To this effect, we add a conditional at the beginning to serve as the terminating condition such that when the number of remaining elements is less than or equal to the specified size, we simply return all the remaining elements as one group.
+*/
+function chunk(array, size) {
+  if (array.length <= size) {
+    return [array];
+  }
+  return [array.slice(0, size), ...chunk(array.slice(size), size)];
+}
+/*
+Notice that we use the .slice() method here to extract portions of the array as chunks as well as reduce the size of the original array continuously until the terminating condition is met.
+*/
 
 module.exports = chunk;
