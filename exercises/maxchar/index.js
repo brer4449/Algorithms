9a7c0d66de399baa425c164bc10f386bf2693421
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-09-11 09:59:17 -0500

    solved!

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// Solution 1 (his solution)
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+  for (let char of str) {
+    // the first part of statement is undefined + 1 which would be null, so either it's 1 or it's adding to what already exists (vid 19.)
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  // for...in... loops used for OBJECTS, for...of... loops used for STRINGS & ARRAYS
+  // in the for...in... loop char is the KEYS not the values
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
