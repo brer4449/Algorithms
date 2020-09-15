cd406250107c0b736cd950ff2a8fb4310a119e9f
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-09-15 10:54:11 -0500

    solved steps!

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,45 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// My solution
+// function steps(n) {
+//   for (let i = 1; i <= n; i++) {
+//     console.log(`${"#".repeat(i)}${" ".repeat(n - i)}`);
+//   }
+// }
+
+// Solution 1 (his way done on own with pseudo code)
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+//     for (let col = 0; col < n; col++) {
+//       if (col <= row) {
+//         stair += "#";
+//       } else {
+//         stair += " ";
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
+
+// Solution 2 using recursion!
+function steps(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+  steps(n, row, stair);
+}
 
 module.exports = steps;
