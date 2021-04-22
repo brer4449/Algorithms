2d079ea506a48ee11cde6571fa6e7c27870c4616
brer4449 <58540640+brer4449@users.noreply.github.com>
2021-04-22 15:30:34 -0400

    moved chunk solutions from capitalize to chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -24,14 +24,40 @@
 // }
 
 // Solution 2
+// function chunk(array, size) {
+//   const chunked = []
+//   let index = 0
+//   while(index < array.length){
+//     chunked.push(array.slice(index, index + size))
+//     index += size
+//   }
+//   return chunked
+// }
+
+// Solution 3 (found on the interwebz) using sPlice
+// function chunk(array, size) {
+//   const chunked = [];
+//   let arrayCopy = [...array];
+//   // let idx = 0;
+//   while (0 < arrayCopy.length) {
+//     chunked.push(arrayCopy.splice(0, size));
+//   }
+//   return chunked;
+// }
+
+// Solution 4 (found on the interwebz) using recursion
+/*
+In this approach, we consistently reduce the problem set(i.e the size of the array) by slicing off chunks of the required size until all elements within the array have been grouped. To this effect, we add a conditional at the beginning to serve as the terminating condition such that when the number of remaining elements is less than or equal to the specified size, we simply return all the remaining elements as one group.
+*/
 function chunk(array, size) {
-  const chunked = []
-  let index = 0
-  while(index < array.length){
-    chunked.push(array.slice(index, index + size))
-    index += size
+  if (array.length <= size) {
+    return [array];
   }
-  return chunked
+  return [array.slice(0, size), ...chunk(array.slice(size), size)];
 }
+/*
+Notice that we use the .slice() method here to extract portions of the array as chunks as well as reduce the size of the original array continuously until the terminating condition is met.
+*/
+// https://scotch.io/courses/the-ultimate-guide-to-javascript-algorithms/array-chunking
 
 module.exports = chunk;
